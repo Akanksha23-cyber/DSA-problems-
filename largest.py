dabe7c118c89f49992dac0539d914f2ea703da4f
Akanksha Keshri <sbu2400946@sburanchi.ac.in>
2026-09-20 15:37:44 +0530

    Add largest solution with images

--- largest.py
+++ largest.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Solution():
     def largest(self,arr):
         largest = arr[0]
@@ -5,3 +6,12 @@ class Solution():
             if arr[i] > largest:
                 largest = arr[i]
         return largest
+=======
+class Solution():
+    def largest(self,arr):
+        largest = arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] > largest:
+                largest = arr[i]
+        return largest
+>>>>>>> d3d48ad (Update largest element solution with images)
