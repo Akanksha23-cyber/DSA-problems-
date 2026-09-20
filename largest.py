4b534e6e1760d05fcd238bda47832b2b23012dbd
Akanksha Keshri <sbu2400946@sburanchi.ac.in>
2026-09-20 15:25:02 +0530

    Implement function to find the largest number in array

---
+++ largest.py
@@ -0,0 +1,7 @@
+class Solution():
+    def largest(self,arr):
+        largest = arr[0]
+        for i in range(1, len(arr)):
+            if arr[i] > largest:
+                largest = arr[i]
+        return largest
